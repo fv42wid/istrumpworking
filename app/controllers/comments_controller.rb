bcdaef1b2fd210502c9efe78ee2051843804c3fa
Frank Vinci <fv42wid@yahoo.com>
2016-12-09 17:22:51 +0000

    create comment on update, show them now

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,11 @@ class CommentsController < ApplicationController
     @comment = current_user.comments.build(comment_params)
     if @comment.save
       flash[:success] = "Comment posted!"
-      redirect_to @comment.commentable
+      if @comment.commentable_type == "Update"
+        redirect_to issue_update_path(@comment.commentable.issue, @comment.commentable)
+      else
+        redirect_to @comment.commentable
+      end
     else
       flash[:danger] = "Something went wrong!"
       redirect_to @comment.commentable
